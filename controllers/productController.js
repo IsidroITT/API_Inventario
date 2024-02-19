691a0b8204b358f18cfd4ce4c7611d4b1af56b0d
Isidro Flores <isanfloresfa@ittepic.edu.mx>
2024-02-19 08:23:19 -0700

    E: Controladores y rutas terminadas, aun no han sido probadas

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,26 +1,39 @@
-const product = require('../models/productsModel');
 const productModel = require('../models/productsModel');
 
 let productsArray = [];
 
-// Get fucntions
+//-----------------------------------------------------
+// Get functions
 function getProducts(){
     return productsArray;
 }
 
 function getProductById(id){
-    return productsArray.find(product => product.id == parseInt(id));
+    return productsArray.find(product => 
+        product.id == parseInt(id));
 }
-// Post fucntions
+
+function getProductByName(name){
+    const product = productsArray.filter(product => 
+        product.name.toLowerCase().includes(name.toLowerCase()));
+
+    return product;
+}
+
+//-----------------------------------------------------
+// Post functions
 function addProduct(name, price, description, stock){
     const newProduct = new productModel(productsArray.length + 1, name, price, description, stock);
     productsArray.push(newProduct);
     return newProduct;
 }
 
+//-----------------------------------------------------
 // Put functions
 function updateProduct(id, name, price, description, stock){
-    const productUpdated = productsArray.find(product => product.id === parseInt(id));
+    const productUpdated = productsArray.find(product => 
+        product.id === parseInt(id));
+
     if(!productUpdated){
         return false;
     }
@@ -33,7 +46,8 @@ function updateProduct(id, name, price, description, stock){
     return productUpdated;
 }
 
-// Delete fucntions
+//-----------------------------------------------------
+// Delete functions
 function deleteProduct(id){
     const productDeleted = productsArray.find(product => product.id === parseInt(id));
     if(!productUpdate){
@@ -45,10 +59,44 @@ function deleteProduct(id){
     return productsArray;
 }
   
+//-----------------------------------------------------
+// Aditional functions
+// Cálculo del valor total del inventario: La API debe proporcionar una ruta para calcular el valor total del inventario, es decir, la suma del precio de cada producto multiplicado por su cantidad en stock.
+function totalInventory(){
+    let total = 0;
+    productsArray.forEach(product => {
+        total += product.price * product.stock;
+    });
+    return total;
+}
+
+// Ordenar productos por precio: Los usuarios deben poder obtener una lista de productos ordenados por precio, ya sea ascendente o descendente.
+function sortProductsByPrice(order){
+    if(order === 'asc'){
+        return productsArray.sort((a, b) => a.price - b.price);
+    } else if(order === 'desc'){
+        return productsArray.sort((a, b) => b.price - a.price);
+    }
+}
+
+// Filtrar productos por cantidad en stock: Los usuarios deben poder obtener una lista de productos que cumplan con ciertos criterios de cantidad en stock, por ejemplo, productos con una cantidad en stock mayor que un valor específico.
+function filterProductsByStock(stock){
+    return productsArray.filter(product => product.stock > stock);
+}
+
+function filterProductsByPrice(price){
+    return productsArray.filter(product => product.price > price);
+}
+
 module.exports = {
     getProducts,
     getProductById,
+    getProductByName,
     addProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    totalInventory,
+    sortProductsByPrice,
+    filterProductsByStock,
+    filterProductsByPrice
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,14 +2,112 @@ const express = require('express');
 const router = express.Router();
 const productController = require("../controllers/productController");
 
+//-----------------------------------------------------
+// GET routes
 router.get("/", (req, res) => { 
   res.status(200).json(productController.getProducts());
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const product = productController.getProductById(id);
+
+  if (product) {
+    res.status(404).json({ message: "Product not found" });
+  }
+
+  return res.status(200).json(product);
+});
+
+router.get("/search", (req, res) => {
+  const { name } = req.query;
+  const product = productController.getProductByName(name);
+
+  if (product) {
+    res.status(404).json({ message: "Product not found" });
+  }
+
+  return res.status(200).json(product);
+});
+
+// -----------------------------------------------------
+// POST routes
 router.post("/", (req, res) => {
   const { name, price, description, stock } = req.body;
   const newProduct = productController.addProduct(name, price, description, stock);
   res.status(201).json(newProduct);
+});
+
+// -----------------------------------------------------
+// PUT routes
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, price, description, stock } = req.body;
+  const updatedProduct = productController.updateProduct(id, name, price, description, stock);
+
+  if (!updatedProduct) {
+    res.status(404).json({ message: "Product not found" });
+  }
+
+  return res.status(200).json(updatedProduct);
+});
+
+// -----------------------------------------------------
+// DELETE routes
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = productController.deleteProduct(id);
+
+  if (!deletedProduct) {
+    res.status(404).json({ message: "Product not found" });
+  }
+
+  return res.status(200).json(deletedProduct);
+});
+
+// -----------------------------------------------------
+// Additional routes
+router.get("/inventory", (req, res) =>{
+  const totalInventory = productController.totalInventory();
+
+  if(!totalInventory){
+    res.status(400).json({ message: "Empty inventory"});
+  }
+
+  return res.status(200).json({ "Total inventory": totalInventory });
+});
+
+router.get("/search/:order", (req, res) => {
+  const { orderParam } = req.params;
+  const order = productController.orderByPrice(orderParam);
+
+  if(!order){
+    res.status(400).json({ message: "Empty inventory"});
+  }
+
+  return res.status(200).json(order);
+});
+
+router.get("/filter/:price", (req, res) => {
+  const { priceParam } = req.params;
+  const filter = productController.filterByPrice(priceParam);
+
+  if(!filter){
+    res.status(400).json({ message: "Empty inventory"});
+  }
+
+  return res.status(200).json(filter);
+});
+
+router.get("/filter/:stock", (req, res) => {
+  const { stockParam } = req.params;
+  const filter = productController.filterByStock(stockParam);
+
+  if(!filter){
+    res.status(400).json({ message: "Empty inventory"});
+  }
+
+  return res.status(200).json(filter);
 });
 
 module.exports = router
